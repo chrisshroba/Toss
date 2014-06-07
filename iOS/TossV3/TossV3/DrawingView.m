267b0cd2bd225f6cbdb20641964ef18c824f528a
bladechapman <your_email@example.com>
2014-06-06 23:13:00 -0700

    added placeholder text

--- iOS/TossV3/TossV3/DrawingView.m
+++ iOS/TossV3/TossV3/DrawingView.m
@@ -14,18 +14,34 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        [self setBackgroundColor:[UIColor clearColor]];
     }
     return self;
 }
 
-/*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+//    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGRect bounds = [self bounds];
+
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width/2.0;
+    center.y = bounds.origin.x + bounds.size.height/2.0;
+
+    NSString *str = @"Placeholder: Drawing will be done on this view";
+    UIFont *font = [UIFont boldSystemFontOfSize:12];
+    CGRect textRect;
+    textRect.size = [str sizeWithFont:font];
+
+    textRect.origin.x = center.x - textRect.size.width/2.0;
+    textRect.origin.y = center.y - textRect.size.height/2.0;
+
+    [[UIColor blackColor] setFill];
+
+    [str drawInRect:textRect withFont:font];
 }
-*/
 
 @end
